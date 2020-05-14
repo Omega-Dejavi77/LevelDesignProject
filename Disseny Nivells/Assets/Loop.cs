452d28650c92bb8ef41beaa5ed7207e93e0816a1
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-14 19:32:23 +0200

    project created

---
+++ Disseny Nivells/Assets/Loop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private float _speed;
+    private float _angularSpeed;
+    public Vector3 force;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        force = transform.eulerAngles += new Vector3(0 ,0, 1) * (Time.deltaTime * 100f);
+
+    }
+}
---
+++ Disseny Nivells/Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody _rb;
+    private bool _attached;
+    private Vector3 force;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        if (!_attached)
+            transform.Translate(new Vector2(x, y) * (Time.deltaTime * 2f));
+        
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _attached = false;
+            transform.SetParent(null);
+            Debug.Log("HOLA");
+            _rb.useGravity = true;
+            _rb.AddRelativeForce(force * 20f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Attach"))
+        {
+            transform.SetParent(other.transform);
+            _attached = true;
+            force = other.transform.GetComponent<Loop>().force;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.collider.CompareTag("Attach"))
+        {
+            transform.SetParent(null);
+            _attached = false;
+        }
+    }
+}
