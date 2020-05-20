cdbde2ab82ebf5e5844a989580718cd167049972
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-20 21:15:10 +0200

    level almost finished

---
+++ Disseny Nivells/Assets/Scripts/Piston.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston : MonoBehaviour
+{
+
+    [SerializeField] private Transform initialPoint;
+    [SerializeField] private float speedFactor;
+    private float _direction = -1f;
+    [SerializeField] private Transform point;
+
+    private void Update()
+    {
+        var distance = Mathf.Abs(point.position.y - initialPoint.position.y);
+        if (distance < 0.1f)
+            _direction *= -1;
+    }
+
+    void FixedUpdate()
+    {
+        var hit = Physics2D.Raycast(point.position, Vector2.down, 200f);
+        print(hit.collider.name);
+        if (hit.collider.CompareTag("Ground"))
+        {
+            var distance = Mathf.Abs(hit.point.y - point.position.y);
+            if (distance < 0.1f)
+            {
+                _direction *= -1;
+                print("pene");
+            }
+        }
+        transform.position += new Vector3(0 , _direction, 0) * (Time.deltaTime * speedFactor);
+    }
+}
---
+++ Disseny Nivells/Assets/Scripts/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] private Transform leftPoint;
+    [SerializeField] private Transform rightPoint;
+    [SerializeField] private float speedFactor;
+    private float _direction = 1f;
+    
+
+    void Update()
+    {
+        if (Distance(leftPoint.position) < 0.2f || Distance(rightPoint.position) < 0.2f)
+            _direction *= -1;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(_direction , 0, 0) * (Time.deltaTime * speedFactor);
+    }
+
+    private float Distance(Vector3 position)
+    {
+        return Mathf.Abs(position.x - transform.position.x);
+    }
+}
