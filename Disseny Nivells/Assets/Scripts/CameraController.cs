d5ab9f114e0965419685bf724ac72d3e9f916049
Javier de la Serna Martínez <37005341+Omega-Dejavi77@users.noreply.github.com>
2020-05-21 20:26:56 +0200

    finished

---
+++ Disseny Nivells/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private void FixedUpdate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(horizontal, vertical, 0) * (speed * Time.deltaTime);
+    }
+}
